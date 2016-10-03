3451f4c148128e9e0d95b3c5c3ba8ef26b593202
Jason Stajich <jason@bioperl.org>
2016-10-03 16:25:50 -0700

    now by default the makeunaln takes care of generating the marker name list file

--- jobs/03_makeunaln.sh
+++ jobs/03_makeunaln.sh
@@ -9,6 +9,13 @@ SEARCH=search
 mkdir -p $ALN/$MARKER
 perl scripts/construct_unaln_files_cdbfasta.pl -d $SEARCH/$MARKER -db pep -o $ALN/$MARKER -ext aa.fasta
 
+LIST=alnlist.$MARKER # this is the list file
+if [ ! -f $LIST ]; then
+ pushd aln/$MARKER
+ ls *.aa.fasta > ../../$LIST
+ popd
+fi
+
 if [ -d cds ]; then
  perl scripts/construct_unaln_files_cdbfasta.pl -d $SEARCH/$MARKER -db cds -o $ALN/$MARKER -ext cds.fasta
 fi 
