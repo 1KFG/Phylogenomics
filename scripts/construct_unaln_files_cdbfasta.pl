0502a34a5c61c5ff9f816a0fee0c779e01d5e56d
Jason Stajich <jason@bioperl.org>
2017-01-16 23:42:59 -0800

    some debugging

--- scripts/construct_unaln_files_cdbfasta.pl
+++ scripts/construct_unaln_files_cdbfasta.pl
@@ -3,14 +3,16 @@ use strict;
 use warnings;
 use Getopt::Long;
 use File::Spec;
+use Bio::SeqIO;
 my $dir = 'search';
 my $dbdir = 'pep';
 my $ext = "fa";
 my $outdir = 'aln';
 my $idxfile = 'allseq';
 my $MIN_COUNT = 4;
-
+my $debug = 0;
 GetOptions(
+    'v|debug!'  => \$debug,
     'd|dir:s'   => \$dir,
     'e|ext:s'   => \$ext,
     'I|idx:s'   => \$idxfile,
@@ -43,7 +45,26 @@ for my $gene ( keys %by_gene ) {
      next;
     }
     open(CDBYANK,"| cdbyank $idxfile.cidx -o $outdir/$gene.$ext") || die $!;    
+    my $expected = 0;
     while( my ($sp,$seqname) = each %{$by_gene{$gene}} ) {
 	print CDBYANK $seqname,"\n";
+        $expected++;
+    }
+    close(CDBYANK);
+    my $nct = `grep -c '^>' $outdir/$gene.$ext`;
+    if( $nct != $expected ) { 
+     my $seqcheck = Bio::SeqIO->new(-format => 'fasta', -file => "$outdir/$gene.$ext");
+     my %seen;
+     while (my $s = $seqcheck->next_seq ) {
+      $seen{$s->display_id}++; 
+      warn("storing ",$s->display_id,"\n");
+     }
+     for my $sp ( keys %{$by_gene{$gene}} ) {
+      my $name = $by_gene{$gene}->{$sp};
+      if( ! exists $seen{$name} ) {	
+	warn("cannot find '$name' in $gene file\n");
+	}
+    }
+	exit;
     }
 }
