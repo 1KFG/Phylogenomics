509102379ac409a470fdbd1650903f758c47d949
Jason Stajich <jason@bioperl.org>
2017-01-20 23:47:37 -0800

    do arrayjobs properly

--- jobs/04_MSA_aln.sh
+++ jobs/04_MSA_aln.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
+
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --job-name=hmmalign
+#SBATCH --job-name=MSA
 #SBATCH --time=3:00:00
 #SBATCH --mem-per-cpu=2G
 #SBATCH --output=MSA.%A_%a.out
@@ -11,15 +12,6 @@ module load trimal
 module load muscle
 module load BMGE
 module load java
-N=$SLURM_ARRAY_TASK_ID
-if [ ! $N ]; then
-  N=$1
-fi
-
-if [ ! $N ]; then
- echo "need to have a job id"
- exit;
-fi
 
 if [ -f config.txt ]; then
  source config.txt
@@ -34,17 +26,19 @@ fi
 
 DIR=aln/$HMM
 LIST=alnlist.$HMM # this is the list file
-N=$PBS_ARRAYID
+
+N=$SLURM_ARRAY_TASK_ID
 if [ ! $N ]; then
   N=$1
 fi
 
-if [ ! $N ]; then
+ if [ ! $N ]; then
  echo "need to have a job id"
  exit;
 fi
 
 G=$(sed -n ${N}p $LIST)
+base=$(basename $G .aa.fasta)
 
 if [ ! -f $DIR/$base.denovo.aln ]; then
  muscle -in $DIR/$G -out $DIR/$base.denovo.aln -quiet
