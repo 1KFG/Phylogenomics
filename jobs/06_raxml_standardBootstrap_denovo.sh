24c817fcf7e737c6be594313781c88adf12d3aab
Jason Stajich <jasonstajich.phd@gmail.com>
2017-01-23 22:20:12 -0800

    add a more definitive prefix and run on slurm

--- jobs/06_raxml_standardBootstrap_denovo.sh
+++ jobs/06_raxml_standardBootstrap_denovo.sh
@@ -1,4 +1,29 @@
-#PBS -l nodes=1:ppn=32,mem=64gb -q highmem -N raxmlAVX.denovo -j oe
+#!/usr/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --job-name=raxmlAVX
+#SBATCH --time=7-0:00:00
+#SBATCH --mem-per-cpu=3G
+#SBATCH --output=raxmlAVX.%A_%a.out
+
+module load RAxML
+
+CPU=2
+
+RUNFOLDER=phylo
+if [ $SLURM_CPUS_ON_NODE ]; then
+ CPU=$SLURM_CPUS_ON_NODE
+fi
+
+if [ -f config.txt ]; then
+ source config.txt
+else
+ PREFIX=ALL
+ FINALPREF=1KFG
+ OUT=Pult
+ EXTRARAXML=
+fi
+
 module load RAxML
 
 CPU=2
@@ -16,7 +41,8 @@ fi
 count=`wc -l expected | awk '{print $1}'`
 datestr=`date +%Y_%b_%d`
 str=$datestr".denovo.JGI1086".${count}sp
-IN=all_${count}.denovo.JGI_1086
+IN=all_${count}.denovo.$HMM
+PREFIX=$PREFIX.$str
 if [ ! -f phylo/$str.fasaln ]; then
  cp $IN.fasaln phylo/$str.fasaln
  cp $IN.partitions.txt phylo/$str.partitions
@@ -24,6 +50,4 @@ if [ ! -f phylo/$str.fasaln ]; then
 fi
 cd phylo
 
-#raxmlHPC-PTHREADS-AVX -T $CPU -f a -x 227 -p 771 -o $OUT -m PROTGAMMAAUTO \
-#  -s $str.fasaln -n $PREFIX -N autoMRE 
 raxmlHPC-PTHREADS-AVX -T $CPU -f a -x 227 -p 771 -o $OUT -m PROTGAMMAAUTO -s $str.fasaln -n $PREFIX -N autoMRE
