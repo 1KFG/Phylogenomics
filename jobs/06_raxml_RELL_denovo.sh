094283b1705d0802f34eb192226aff48cc2ac859
Jason Stajich <jasonstajich.phd@gmail.com>
2017-02-04 22:21:44 -0800

    Handle denovo alignments (MSA from MUSCLE) as well

--- jobs/06_raxml_RELL_denovo.sh
+++ jobs/06_raxml_RELL_denovo.sh
@@ -1,29 +1,39 @@
-#PBS -l nodes=1:ppn=32,mem=64gb -q highmem -N raxmlAVX.denovo -j oe
+#!/usr/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --job-name=raxmlRELLdenovo
+#SBATCH --time=7-0:00:00
+#SBATCH --mem-per-cpu=3G
+#SBATCH --output=raxmlRELLdenovo.%A.out
+
 module load RAxML
 
 CPU=2
 
-if [ $PBS_NUM_PPN ]; then
- CPU=$PBS_NUM_PPN
+RUNFOLDER=phylo
+if [ $SLURM_CPUS_ON_NODE ]; then
+ CPU=$SLURM_CPUS_ON_NODE
 fi
+
 if [ -f config.txt ]; then
  source config.txt
 else
  PREFIX=ALL
  FINALPREF=1KFG
  OUT=Pult
+ EXTRARAXML=
 fi
 count=`wc -l expected | awk '{print $1}'`
 datestr=`date +%Y_%b_%d`
-str=$datestr".denovo.JGI1086".${count}sp
-IN=all_${count}.denovo.JGI_1086
+str=$PREFIX.$datestr.denovo.$HMM.${count}sp
+IN=all_${count}.denovo.$HMM
+FINALPREF=$str
+mkdir -p phylo
 if [ ! -f phylo/$str.fasaln ]; then
  cp $IN.fasaln phylo/$str.fasaln
  cp $IN.partitions.txt phylo/$str.partitions
- cp $IN.phy phylo/$str.phy
 fi
-cd phylo
 
-#raxmlHPC-PTHREADS-AVX -T $CPU -f a -x 227 -p 771 -o $OUT -m PROTGAMMAAUTO \
-#  -s $str.fasaln -n $PREFIX -N autoMRE 
-raxmlHPC-PTHREADS-AVX -T $CPU -f D -p 771 -o $OUT -m PROTGAMMAAUTO -s $str.fasaln -n $str
+cd phylo
+raxmlHPC-PTHREADS-AVX -T $CPU -f D -p 771 -o $OUT -m PROTGAMMALG \
+  -s $str.fasaln -n ${FINALPREF}_RELL
---
+++ jobs/06_raxml_RELL_mapBS_denovo.sh
@@ -0,0 +1,20 @@
+#PBS -l nodes=1:ppn=1 -N raxmlRELL.BS -j oe
+module load RAxML
+
+CPU=1
+if [ -f config.txt ]; then
+ source config.txt
+else
+ PREFIX=ALL
+ FINALPREF=1KFG
+ OUT=Pult
+fi
+count=`wc -l expected | awk '{print $1}'`
+datestr=`date +%Y_%b_%d`
+str=${PREFIX}.${datestr}.denovo.${HMM}.${count}sp
+IN=all_${count}.denovo.${HMM}
+FINALPREF=$str
+
+cd phylo
+raxmlHPC-AVX -f b -m PROTGAMMALG -t RAxML_bestTree.RELL.${FINALPREF} \
+ -z RAxML_rellBootstrap.RELL.${FINALPREF} -n ${FINALPREF}_RELL_BS
