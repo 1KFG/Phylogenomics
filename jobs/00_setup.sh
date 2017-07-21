dc28e0eba0241923bf584decdd0b619ce2404f91
Jason Stajich <jasonstajich.phd@gmail.com>
2017-07-21 15:15:17 -0700

    make a all-in-one setup

---
+++ jobs/00_setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+if [ ! -f config.txt ]; then
+ echo "Need a config.txt file"
+ exit
+fi
+
+source ./config.txt
+
+if [ ! -d HMM/$HMM  ]; then 
+ echo "need a HMM defined in config.txt and this needs to be a folder in HMM/"
+ echo "usually you checkout / download https://github.com/1KFG/Phylogenomics_HMMs/releases/latest and then "
+ echo "symlink ln -s ../Phylogenomics_HMMs/HMM ./HMM"
+ exit
+fi
+
+echo "make expected prefixes"
+bash jobs/make_expected_file.sh
+echo "making prefix.tab"
+perl scripts/make_prefixes_readdata.pl pep > prefix.tab
+echo "makeing list file for hmmsearch runs"
+bash jobs/make_peplist.sh
